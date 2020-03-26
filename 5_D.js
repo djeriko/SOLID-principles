ad388273e1154024723d936d29ea57dfa4129eca
djeriko <jerikosalivan@gmail.com>
2020-03-26 13:06:24 +0900

    Dependency Invertion Principle

---
+++ 5_D.js
@@ -0,0 +1,53 @@
+// Dependency Inversion Principle
+
+class Fetch {
+  request() {
+    // return fetch(url).then(r => r.json())
+    return Promise.resolve("data from fetch")
+  }
+}
+
+class LocalStorage {
+  get() {
+    const dataFromLocalStorage = "data from local storage"
+    return dataFromLocalStorage
+  }
+}
+
+class FetchClient {
+  constructor() {
+    this.fetch = new Fetch()
+  }
+
+  clientGet() {
+    return this.fetch.request('vk.com') 
+  }
+}
+
+class LocalStorageClient {
+  constructor() {
+    this.localStorage = new LocalStorage
+  }
+
+  clientGet() {
+    return this.localStorage.get()
+  }
+}
+
+class Database {
+  constructor(client) {
+    this.client = client
+  }
+
+  getData(key) {
+    return this.client.clientGet(key)
+  }
+}
+
+
+// const db = new Database(new FetchClient())
+const db = new Database(new LocalStorageClient())
+
+console.log(db.getData('rand'))
+
+
